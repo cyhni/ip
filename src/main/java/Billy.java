2e60098518bf14a69f4cda71d0cc6c316469dfa0
cyhni <nigel99_chan@hotmail.com>
2025-01-23 22:27:08 +0800

    Added Billy.parseCommand(String userCmd) and Billy.list()
    Billy.parseCommand(String userCmd) default action is to add to the String array containing the list.
    If list is inputted, it will call Billy.list() to display the current list.

--- src/main/java/Billy.java
+++ src/main/java/Billy.java
@@ -2,6 +2,8 @@ import constants.DesignConstants;
 import java.util.Scanner;
 
 public class Billy {
+    private static String[] list = new String[100];  
+    private static int counter = 0;
     public static void main(String[] args) {
         Billy.introduction();
         Scanner scanner = new Scanner(System.in);
@@ -14,8 +16,7 @@ public class Billy {
                 break;
             }
 
-            System.out.println("\n" + userCmd + "\n");
-            System.out.println(DesignConstants.HORIZONTALLINE_STRING);
+            Billy.parseCommand(userCmd);
         }
         scanner.close();
         Billy.bye();
@@ -31,6 +32,29 @@ public class Billy {
 
     private final static void bye() {
         System.out.println("\nBye bye.\n");
+        System.out.println(DesignConstants.HORIZONTALLINE_STRING);
+    }
+
+    private static void parseCommand(String userCmd) {
+
+        switch (userCmd) {
+            case "list":
+                Billy.list();
+                break;
+            default:
+                list[counter] = userCmd;
+                counter++;
+                System.out.println("\nAdded to the list: " + userCmd + "\n");
+                System.out.println(DesignConstants.HORIZONTALLINE_STRING);
+        }
+    }
+
+    private static void list() {
+        System.out.println("\nHere are the items in your list:");
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+        System.out.println();
         System.out.println(DesignConstants.HORIZONTALLINE_STRING);
     }
 }
