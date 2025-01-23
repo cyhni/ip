b2dd861a77450f1960285f7ddd36ba35a50226bb
cyhni <nigel99_chan@hotmail.com>
2025-01-23 13:37:31 +0800

    Updated chatbot name and abstracted string logo to constants folder
    Chatbot name updated to Billy. Logo design abstracted to a constant file that will contain all the design related string constants.

---
+++ src/main/java/Billy.java
@@ -0,0 +1,7 @@
+import constants.DesignConstants;
+
+public class Billy {
+    public static void main(String[] args) {
+        System.out.println("Hello from\n" + DesignConstants.LOGO_STRING);
+    }
+}
